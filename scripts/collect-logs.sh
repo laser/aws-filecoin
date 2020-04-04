ec0092fdb23762fefd0e41b6fe021f74f6edf21e
laser <l@s3r.me>
2020-04-04 12:19:50 -0700

    copy log files from nodes which didn't sync chain

--- scripts/collect-logs.sh
+++ scripts/collect-logs.sh
@@ -16,11 +16,20 @@ mkdir -p ${root_dir}
 
 for ip_addr in ${ip_addresses[@]}; do
     miner_id=$(curl -s "https://kvdb.io/${kvdb_bucket}/${kvdb_prefix}_${ip_addr}")
-    miner_dir="${root_dir}/${miner_id}"
-    mkdir -p ${miner_dir}
 
-    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "ubuntu@${ip_addr}:/var/log/daemon.log" "${miner_dir}/daemon.log"
-    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "ubuntu@${ip_addr}:/var/log/miner.log" "${miner_dir}/miner.log"
+    if [[ "${miner_id}" = "Not Found" ]]; then
+        miner_dir="${root_dir}/did-not-join-cluster-${ip_addr}"
+        mkdir -p ${miner_dir}
+
+        scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "ubuntu@${ip_addr}:/var/log/daemon.log" "${miner_dir}/daemon.log"
+        scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "ubuntu@${ip_addr}:/var/log/miner.log" "${miner_dir}/miner.log"
+    else
+        miner_dir="${root_dir}/${miner_id}"
+        mkdir -p ${miner_dir}
+
+        scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "ubuntu@${ip_addr}:/var/log/daemon.log" "${miner_dir}/daemon.log"
+        scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "ubuntu@${ip_addr}:/var/log/miner.log" "${miner_dir}/miner.log"
+    fi
 done
 
 echo "done: ${root_dir}"
